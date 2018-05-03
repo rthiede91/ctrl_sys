b2eb79fb72b7457fe5807a6b39015f016bafbc99
rthiede91 <rthiede91@gmail.com>
2018-05-03 09:22:43 -0300

    socket read

--- ctrl_sys/Client User/User Control/src/user/control/socket/readConnection.java
+++ ctrl_sys/Client User/User Control/src/user/control/socket/readConnection.java
@@ -4,12 +4,29 @@
  * and open the template in the editor.
  */
 package user.control.socket;
-
+import java.io.InputStream;
+import java.util.Scanner;
 /**
  *
  * @author Thiedes
  */
-public class readConnection 
+public class readConnection implements Runnable 
 {
-    
+    private InputStream servidor;
+
+    public readConnection(InputStream servidor) 
+    {
+            this.servidor = servidor;
+    }
+
+    public void run() 
+    {
+            try(Scanner s = new Scanner(this.servidor))
+            {
+                    while (s.hasNextLine()) 
+                    {
+                            System.out.println(s.nextLine());
+                    }
+            }
+    }
 }
